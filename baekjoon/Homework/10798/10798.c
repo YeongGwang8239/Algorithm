ae35647761b5ffc0646133ff059aa4d719b68581
YeongGwang8239 <yeonggwang8239@naver.com>
2024-04-18 00:27:08 +0900

    B1 5 solved

---
+++ baekjoon/Homework/10798/10798.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+	char str[5][15] = { 0 };
+
+	for (int i = 0; i < 5; i++) {
+		scanf("%s", str[i]);
+	}
+
+	for (int i = 0; i < 15; i++) {
+		for (int j = 0; j < 5; j++) {
+			if (str[j][i]!='\0') {
+				printf("%c", str[j][i]);
+			}
+		}
+	}
+
+	return 0;
+}
