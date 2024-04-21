956facb7af8240c84d6d3298e86ccccb5d2c780c
YeongGwang8239 <yeonggwang8239@naver.com>
2024-04-21 23:12:06 +0900

    11:11AM

--- baekjoon/main.c
+++ baekjoon/main.c
@@ -2,43 +2,22 @@
 #include <stdlib.h>
 
 int main() {
-    int n, m, i, j, count_factor, count_prime, d=0, sum=0;
-    scanf("%d", &n);
-    scanf("%d", &m);
-    for (i=n; i<=m; i++) {
-        count_factor = 0;
-        count_prime = 0;
-        for (j=2; j<n; j++) {
-            if (n%j==0) {
-                count_factor++;
-                if (count_factor>2) {
-                    break;
-                }
-            }
-            break;
-        }
-        count_prime++;
+    int n, m, i, j, temp;
+    scanf("%d %d", &n, &m);
+    int *arr = (int*)malloc(4*n);
+    for (i=0; i<n; i++) {
+        scanf("%d", &arr[i]);
     }
-
-    int *arr = (int*)malloc(4*count_prime);
-    for (i=n; i<=m; i++) {
-        count_factor = 0;
-        for (j=2; j<n; j++) {
-            if (n%j==0) {
-                count_factor++;
-                if (count_factor>2) {
-                    break;
-                }
-            }
-            break;
-        }
-        arr[d] = i;
-        d++;
-    }
-    for (i=0; i<count_prime; i++) {
-        sum += arr[i];
-    }
-    printf("%d", sum);
+    for (i=0; i<n-1; i++) {
+		for (j=0; j<n-1; j++) {
+			if (arr[j]<arr[j+1]) {
+				temp = arr[j];
+				arr[j] = arr[j+1];
+				arr[j+1] = temp;
+			}
+		}
+	}
+    // 2798 블랙잭
 
     return 0;
 }
