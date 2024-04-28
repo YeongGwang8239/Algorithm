57ac25cc28e83f73649a6ad1bfd71dfaace4d5e5
YeongGwang8239 <yeonggwang8239@naver.com>
2024-04-29 02:14:17 +0900

    2:14AM

--- baekjoon/main.c
+++ baekjoon/main.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    
-
+    int time_int;
+    double time_float, a, b, v;
+    scanf("%lf %lf %lf", &a, &b, &v);
+    time_float = (v-a)/(a-b);
+    time_int = (v-a)/(a-b);
+    if (time_float==time_int) {
+        printf("%d", time_int+1);
+    }
+    else {
+        time_float += 2;
+        time_int = (int)time_float;
+        printf("%d", time_int);
+    }
     return 0;
 }
--- baekjoon/test.c
+++ baekjoon/test.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    
-
-    
+    printf("%d", (0.25==0));
     return 0;
 }
