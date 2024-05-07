6d68f7b45a8d112808f22ab5398b648d3d632934
YeongGwang8239 <yeonggwang8239@naver.com>
2024-05-07 16:18:33 +0900

    4:18PM

--- baekjoon/main.c
+++ baekjoon/main.c
@@ -1,14 +1,8 @@
 #include <stdio.h>
 
-int Cal() {
-    return 1;
-}
-
 int main() {
-    int sum=0;
-    for (int i=0; i<3; i++) {
-        sum += Cal();
-    }
-    printf("%d", sum);
-    return 0;
+	
+
+	
+	return 0;
 }
